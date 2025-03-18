770eac3904e647960be777c69cbbcc3ac9c98388
satvik8373 <satvikpatel8373@gmail.com>
2025-03-18 21:38:03 +0530

    a

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,6 @@
 window.config = {
   API_URL: window.location.hostname === 'localhost' 
     ? 'http://localhost:3000/api'
-    : 'https://mavrix-cafe-api.onrender.com/api'
+    : 'https://mavrix-cafe-api.onrender.com/api',
+  isAdmin: window.location.pathname.includes('admin.html')
 }; 
